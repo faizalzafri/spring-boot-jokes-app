7e901e11e248e114c2947e425df383e5f270e6f0
Faizal <zafri_faizal@outlook.com>
2019-01-30 21:01:46 +0530

    added xml configuration

--- src/main/java/com/faizal/springbootjokesapp/SpringBootJokesAppApplication.java
+++ src/main/java/com/faizal/springbootjokesapp/SpringBootJokesAppApplication.java
@@ -2,8 +2,11 @@ package com.faizal.springbootjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
+
 public class SpringBootJokesAppApplication {
 
 	public static void main(String[] args) {
@@ -11,4 +14,3 @@ public class SpringBootJokesAppApplication {
 	}
 
 }
-
